9396d42376fbd43be0d3096bdb6c135ed8f99b76
Влад Кантамиров <kantamirov2016@gmail.com>
2024-03-25 13:45:18 +0300

    add current date

--- myForm.py
+++ myForm.py
@@ -1,15 +1,19 @@
-from bottle import post, request
+from bottle import post, request #Импорт фреймворка
 import re
+from datetime import datetime
 
 @post('/home', method='post')
 def my_form():
     mail = request.forms.get('ADRESS')
     quest = request.forms.get('QUEST')
+    username = request.forms.get('USERNAME')
+    cur_date = datetime.now().strftime("%Y-%m-%d") # получение текущей даты
 
-    if not mail or not quest:
+
+    if not mail or not quest or not username:
         return "Please fill in all fields"
 
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", mail):
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", mail):#регулярное выражение для проверки почтового адреса
         return "Please enter a valid email address"
 
-    return "Thanks! The answer will be sent to the mail %s" % mail
+    return f"Thanks, {username}! The answer will be sent to the mail {mail}. Access Date : {cur_date}"
