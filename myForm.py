8435e7334cb867d151a1bd8ffeac785dfe293b75
Влад Кантамиров <kantamirov2016@gmail.com>
2024-04-13 19:31:42 +0300

    add writing to JSON

--- myForm.py
+++ myForm.py
@@ -2,11 +2,20 @@ from bottle import post, request #Импорт фреймворка
 import re
 import pdb
 from datetime import datetime
+import json
+import os
+
+# Путь к файлу
+FILE_PATH = 'data.json'
+
+# Проверка вопроса на соответствие условиям
+def is_valid_question(quest):
+    return len(quest) > 3 and not quest.isdigit()
+
 
 @post('/home', method='post')
 def my_form():
-    questions = {}
-    list = []
+
     mail = request.forms.get('ADRESS')
     quest = request.forms.get('QUEST')
     username = request.forms.get('USERNAME')
@@ -25,10 +34,26 @@ def my_form():
             ")+", mail):#регулярное выражение для проверки почтового адреса
         return "Please enter a valid email address"
     
-    list.append(username)
-    list.append(quest)
-    questions[mail] = list
-    pdb.set_trace()
+    # Загрузка данных из файла или создание нового, если он не существует
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, 'r') as file:
+            data = json.load(file)
+    else:
+        data = {}
 
+    # Проверка наличия почтового адреса в данных
+    if mail not in data:
+        data[mail] = []
+
+    # Проверка вопроса и добавление его в список, если он удовлетворяет условиям
+    if is_valid_question(quest) and quest not in data[mail]:
+        data[mail].append(quest)
+    else :
+        return "Enter a valid question/quest already exists"
+
+    # Запись обновленных данных обратно в файл
+    with open(FILE_PATH, 'w') as file:
+        json.dump(data, file)
+        
     return f"Thanks, {username}! The answer will be sent to the mail {mail}. Access Date : {cur_date}"
     
